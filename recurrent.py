d7c639a68c5f5f09f396ff5bd85ff600ee376896
Han Guo <alex.guohan1106@gmail.com>
2025-04-13 20:17:50 -0400

    [recurrent]: device and dtype of state

--- recurrent.py
+++ recurrent.py
@@ -9,7 +9,7 @@ def linear_attention(
 ) -> torch.Tensor:    # shape: [batch, length, num_heads, num_units]
     # [batch, num_heads, num_units, num_units]
     Y = torch.zeros_like(V)
-    S = torch.zeros([Q.shape[0], Q.shape[2], K.shape[3], V.shape[3]])
+    S = torch.zeros([Q.shape[0], Q.shape[2], K.shape[3], V.shape[3]], dtype=V.dtype, device=V.device)
     for t in range(Q.shape[1]):
         S = S + einsum(K[:, t, ...], V[:, t, ...], "b h dk, b h dv -> b h dk dv")
         Y[:, t, ...] = einsum(Q[:, t, ...], S, "b h dk, b h dk dv -> b h dv")
